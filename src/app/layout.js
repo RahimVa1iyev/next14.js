bd251dc7322451830629e976e7a8d4d05a497c06
Rahim Valiyev <rahimvaliyev99@gmail.com>
2024-10-13 16:08:39 +0400

    feat: Add google-font

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,16 @@ import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css";
 
+import { Josefin_Sans } from "next/font/google"
+
+const josefin = Josefin_Sans({
+  subsets : ["latin"],
+  display : "swap",
+  variable : "--font-josefin",
+  weight : ["400", "500", "700"]
+
+})
+
 
 export const metadata = {
   title: {
@@ -14,7 +24,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen" >
+      <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`} >
       <header>
           <Logo />
           <Navigation />
