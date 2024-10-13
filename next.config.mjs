122901b84a8873d1bba10b6d0eb82fddf0e93d54
Rahim Valiyev <rahimvaliyev99@gmail.com>
2024-10-13 19:02:09 +0400

    feat: Add about,cabins page - configure supabase - fetch get request

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "hvlrqqzumpqklusjwrzh.supabase.co",
+          port: "",
+          pathname: "/storage/v1/object/public/cabins-image/**",
+        },
+      ],
+    },
+    // output: "export",
+  };
+  
+  export default nextConfig;
+  
--- src/app/cabins/page.js
+++ src/app/cabins/page.js
@@ -1,14 +1,37 @@
+import CabinCard from "../_components/CabinCard";
+import { getCabins } from "../_lib/data-service";
 
 export const metadata = {
     title : "Cabins",
     description : "Here are some popular cabins in our resort."
 }
 
-export default function Cabins() {
+export default async function Cabins() {
+    // CHANGE
+    const cabins = await getCabins();
+    console.log(cabins);
+  
     return (
-        <div>
-            <h1>Cabins</h1>
-            <p>Here are some popular cabins in our resort.</p>
-        </div>
-    )
-}
+      <div>
+        <h1 className="text-4xl mb-5 text-accent-400 font-medium">
+          Our Luxury Cabins
+        </h1>
+        <p className="text-primary-200 text-lg mb-10">
+          Cozy yet luxurious cabins, located right in the heart of the Italian
+          Dolomites. Imagine waking up to beautiful mountain views, spending your
+          days exploring the dark forests around, or just relaxing in your private
+          hot tub under the stars. Enjoy nature&apos;s beauty in your own little home
+          away from home. The perfect spot for a peaceful, calm vacation. Welcome
+          to paradise.
+        </p>
+  
+        {cabins.length > 0 && (
+          <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+            {cabins.map((cabin) => (
+              <CabinCard cabin={cabin} key={cabin.id} />
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
